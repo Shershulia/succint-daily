7e1c1b8d775a5e4a8998a09928deeac04619e9a7
Ivan <krol31072002@gmail.com>
2025-07-04 21:18:58 +0200

    Fix logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/logo.png" type="image/png" />
-      </head>
       <body
           className={`${ivory.variable} ${geistSans.variable} antialiased`}
       >
